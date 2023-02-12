3210dfd552783e0f5131dff2bf1472c05d3f0df0
angrywasp <angrywasp@live.com>
2023-02-12 12:59:46 +1100

    Write a test case for AES encryption

--- Src/AngryWasp.Cryptography.Test/Src/Main.cs
+++ Src/AngryWasp.Cryptography.Test/Src/Main.cs
@@ -1,4 +1,6 @@
 using System;
+using System.Text;
+using System.Diagnostics;
 
 namespace AngryWasp.Cryptography.Test
 {
@@ -6,7 +8,14 @@ namespace AngryWasp.Cryptography.Test
     {
         private static void Main(string[] rawArgs)
         {
-
+            var test = "Hello world, this is a random test string that we will attempt to encrypt and decrypt";
+            int length = test.Length;
+            var password = Encoding.ASCII.GetBytes("12345");
+            var encrypted = Aes.Encrypt(Encoding.ASCII.GetBytes(test), password);
+            var decrypt = Aes.Decrypt(encrypted, password, out byte[] decryptedBytes);
+            var decrypted = Encoding.ASCII.GetString(decryptedBytes);
+            Debug.Assert(decrypted == test, "Encryption test failed");
+            
         }
     }
 }
